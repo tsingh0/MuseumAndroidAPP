ca5c620ddff9c49f542643a7aa14b597c7379cdf
kmurdzek <kmurdzek1415@gmail.com>
2020-12-02 16:04:42 -0500

    prices

--- app/src/main/java/com/example/museumapp/MuseumOne.java
+++ app/src/main/java/com/example/museumapp/MuseumOne.java
@@ -2,10 +2,13 @@ package com.example.museumapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MuseumOne extends AppCompatActivity {
@@ -13,12 +16,23 @@ public class MuseumOne extends AppCompatActivity {
     private static double adultTicketPrice;
     private static double studentTicketPrice;
     private static double seniorTicketPrice;
+    private TextView AMNH;
+
+    private Spinner spinner_child;
+    private Spinner spinner_adult;
+    private Spinner spinner_senior;
+    private TextView ticketPrice;
+    private TextView tax;
+    private TextView total;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         switch(option){
             case 1:
                 setContentView(R.layout.activity_museum_one);
+
+
                 break;
             case 2:
                 setContentView(R.layout.activity_museum_two);
@@ -59,6 +73,45 @@ public class MuseumOne extends AppCompatActivity {
                 break;
         }
 
+    }
+    public void calculate(View view){
+        switch(option){
+            case 1:
+                spinner_child = findViewById(R.id.spinner_child);
+                String numberOfChildren = spinner_child.getSelectedItem().toString();
+                spinner_adult = findViewById(R.id.spinner_adult);
+                String numberOfAdults = spinner_adult.getSelectedItem().toString();
+                spinner_senior = findViewById(R.id.spinner_senior);
+                String numberOfSeniors = spinner_senior.getSelectedItem().toString();
+                int children = Integer.parseInt(numberOfChildren);
+                int adults = Integer.parseInt(numberOfAdults);
+                int seniors = Integer.parseInt(numberOfSeniors);
+                double totalOfTickets = children * Prices.childPriceHistory +adults * Prices.adultPriceHistory+ seniors * Prices.seniorPriceHistory;
+                double taxCost = totalOfTickets * 0.08875;
+                double totalCost = totalOfTickets + taxCost;
+
+                ticketPrice = (TextView)findViewById(R.id.ticketPrice);
+                tax = (TextView)findViewById(R.id.tax);
+                total = (TextView)findViewById(R.id.total);
+
+                ticketPrice.setText("$ "+totalOfTickets+ "");
+                tax.setText("$ "+taxCost+"");
+                total.setText("$ " +totalCost+"");
+
+                break;
+            case 2:
+
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            default:
+                break;
+        }
     }
 
 
---
+++ app/src/main/java/com/example/museumapp/Prices.java
@@ -0,0 +1,19 @@
+package com.example.museumapp;
+
+public class Prices {
+    public static final double childPriceMoMa = 0.0;
+    public static final double adultPriceMoMa = 25.0;
+    public static final double seniorPriceMoMa = 17.0;
+
+    public static final double childPriceHistory = 13.0;
+    public static final double adultPriceHistory = 23.0;
+    public static final double seniorPriceHistory = 18.0;
+
+    public static final double childPriceIllusions = 18.0;
+    public static final double adultPriceIllusions = 22.0;
+    public static final double seniorPriceIllusions = 20.0;
+
+    public static final double childPriceMet = 0.0;
+    public static final double adultPriceMet = 25.0;
+    public static final double seniorPriceMet = 17.0;
+}
